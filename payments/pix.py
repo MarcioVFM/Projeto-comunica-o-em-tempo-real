b71a29aedbe29cd6c09193c8383548fea15367f0
Marcio Vinicius <marciov859@gmail.com>
2024-09-18 16:33:33 -0400

    updating app.py, adding new get_image route for qr_code and updating createpayment_pix to insert qr_code and bank infos to obj

--- payments/pix.py
+++ payments/pix.py
@@ -6,12 +6,12 @@ class Pix:
         pass
 
     def create_payment(self):
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f'hash_payment_{bank_payment_id}'
 
         img = qrcode.make(hash_payment)
-        img.save(f'static/img/qr_code_payment{bank_payment_id}.png')
+        img.save(f'static/img/qr_code_payment_{bank_payment_id}.png')
 
         return {'bank_payment_id':bank_payment_id,
-                'qr_code_path': f'bqr_code_payment_{bank_payment_id}'}
+                'qr_code_path': f'qr_code_payment_{bank_payment_id}'}
